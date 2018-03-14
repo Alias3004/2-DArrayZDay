ec308d68e5cfe368272eb2f511379dc40ce2e79f
Colander <281137@bsd48.org>
2018-03-14 15:58:23 -0700

    Update Player.java

--- ZDay2/src/main/java/World/Player.java
+++ ZDay2/src/main/java/World/Player.java
@@ -1,4 +1,30 @@
 package main.java.World;
 
 public class Player extends Actor {
+    private int row;
+    private int col;
+    private String type = "@";
+
+    public Player(int startrow, int startcol){
+        row = startrow;
+        col = startcol;
+    }
+    //Accessors and Mutators
+    public int getRow(){
+        return row;
+    }
+    public int getColumn(){
+        return col;
+    }
+    public void setRow(int newrow){
+        row = newrow;
+    }
+    public void setColumn(int newcol){
+        col = newcol;
+    }
+    public String getType(){
+        return type;
+    }
+
+    public void processActors(){}
 }
